f3252665e530c9f5f2f5eb4d6d57eabd144ed129
safaelbess <136888825+safaelbess@users.noreply.github.com>
2023-07-07 09:53:06 -0700

    initial trial

---
+++ main.py
@@ -0,0 +1,121 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world - how are you?")
+print("hell everyone\n")
+print ("how are you today\n")
+print ("iam very well\n")
+print("this is John's first program\n")
+name="safa"
+name =input ("enter your name\n")
+print ("my name is " )
+
+input(name)
+month =0
+month =input("input the current month\n")
+print ("the current month is")
+print(month)
+
+
+
+
+name="safa"
+age=0
+ 
+name=input("enter your name")
+
+
+age=input("enter your age")
+print("my name is")
+print (name)
+print ("my age is ")
+print (age)
+
+
+
+
+# Online Python Playground
+# Use the online IDE to write, edit & run your Python code
+# Create, edit & delete files online
+
+animal="animal"
+animal=input("enter your favourite animal")
+print("my favourite animal is")
+print (animal)
+
+
+
+# Online Python Playground
+# Use the online IDE to write, edit & run your Python code
+# Create, edit & delete files online
+
+first_name="f_n"
+last_name="l_n"
+first_name=input("enter your first name")
+last_name=input("enter your last name")
+print("what is your first name?",first_name)
+print("what is your last name?",last_name)
+print("hello",first_name,last_name)
+
+
+
+
+country="count"
+pop=0
+country=input("enter the country")
+pop=input("enter the population")
+print(country,"has",pop,"population")
+
+
+
+
+
+num1=0
+num2=0
+sum1=0
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+sum1=num1+num2
+print("the sum is ",sum1)
+
+
+
+num1=0
+num2=0
+
+num1=int(input("what is the first number"))
+num2=int(input("what is the second number"))
+div1=num1/num2
+print(num1,"divide ",num2,"equals",div1)
+
+
+
+
+
+# Online Python Playground
+# Use the online IDE to write, edit & run your Python code
+# Create, edit & delete files online
+
+# Online Python Playground
+# Use the online IDE to write, edit & run your Python code
+# Create, edit & delete files onlin
+num1=0
+num2=0
+num3=0
+sum1=0.0
+num1=int(input("what is the first number"))
+num2=int(input("what is the second number"))
+num3=int(input("enter the third number"))
+sum1=num1+num2
+
+print ("the result is")
+print(sum1 ** num3)
+
+
+
+
+
+
+
+
+
+
